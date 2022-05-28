e4b0db6c2b5476e110a6a3942b0bbc28487e6ca6
M4thieuB <mathieu.baudon@g-mail.fr>
2022-05-28 23:37:04 +0200

    Fix some parameters

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,20 @@
 if status is-interactive
-    alias ll 'exa --icons -gal'
+
+   # Adding important commands to the PATH env variable
+   fish_add_path -mP $HOME/.emacs.d/bin
+   fish_add_path -mP $HOME/scripts
+
+# Defining handy aliases
+   alias ll 'exa --icons -gal --group-directories-first'
+   alias tree 'exa --icons -g --tree --group-directories-first'
+   alias ls='exa -g --icons --group-directories-first'
+
+# Opening emacs through a terminal (I use the client in order to open the GUI faster)
+   alias emacs 'emacsclient -c -a "emacs"'
+
+
+   alias cp 'cp -i'
+   alias rm 'rm -i'
+
+
 end
