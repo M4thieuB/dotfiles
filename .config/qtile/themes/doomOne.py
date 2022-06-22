8f6816ae8e07c9d93e2ed4ad821a30aa50176f4d
M4thieuB <mathieu.baudon@g-mail.fr>
2022-06-22 20:13:11 +0200

    Qtile stuff

---
+++ .config/qtile/themes/doomOne.py
@@ -0,0 +1,20 @@
+colors = [
+    "#282c34", # Background
+    "#bbc2cf", # Foreground
+    "#1c1f24",
+    "#ff6c6b",
+    "#98be65",
+    "#da8548",
+    "#51afef",
+    "#c678dd",
+    "#5699af",
+    "#202328",
+    "#5b6268",
+    "#da8548",
+    "#4db5bd",
+    "#ecbe7b",
+    "#3071db",
+    "#a9a1e1",
+    "#46d9ff",
+    "#dfdfdf"
+]
